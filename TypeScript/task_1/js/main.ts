36c422ba4a829acee7c7d01c5c6768201fe707bd
cosqun2006 <cosqunibrahimov500@gmail.com>
2026-08-16 22:17:05 +0400

    Complete task 4

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -40,3 +40,34 @@ export const printTeacher: printTeacherFunction = (firstName: string, lastName: 
 };
 
 console.log(printTeacher("John", "Doe"));
+
+export interface StudentClassConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+export interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+export class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+const student = new StudentClass('John', 'Doe');
+console.log(student.displayName());
+console.log(student.workOnHomework());
